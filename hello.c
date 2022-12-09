3882a6bae83bbd86e36d7a61f54a950bb1f42b8b
Ravi Prakash Sahu <91124985+31740cse@users.noreply.github.com>
2022-12-09 19:29:06 +0000

    first_commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello ");
+    printf("hello ");
+    printf("hello ");
+    printf("hello ");
+    printf("hello ");
+    printf("hello ");
+    printf("hello ");
+    return 0;
+}
